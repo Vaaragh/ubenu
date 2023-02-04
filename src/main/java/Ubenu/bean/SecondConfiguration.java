94501e91fd151678cc78130696a4139204e79074
Vaaragh <vaarghl@gmail.com>
2023-02-04 22:06:48 +0100

    feat/fix: add localization and refactor for specific locales

--- src/main/java/Ubenu/bean/SecondConfiguration.java
+++ src/main/java/Ubenu/bean/SecondConfiguration.java
@@ -1,32 +1,46 @@
 package Ubenu.bean;
 
 import java.util.HashMap;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class SecondConfiguration {
+public class SecondConfiguration implements WebMvcConfigurer {
 	
-	@Bean(name= {"appMemory"},
-			initMethod="init", destroyMethod="destroy")
-	public ApplicationMemory getApplicationMemory() {
-		return new ApplicationMemory();
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
 	}
 	
-	public class ApplicationMemory extends HashMap {
-		
-		@Override
-		public String toString() {
-			return "ApplicationMemory" + this.hashCode();
-		}
-		
-		public void init() {
-			System.out.println("init method called");
-		}
-		public void destroy() {
-			System.out.println("destroy method called");
-		}
+   @Bean
+    public ResourceBundleMessageSource messageSource() {
+        final ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("static/localization/messages");
+        return source;
 	}
+	   
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
+    }
+	
 
 }
